ad48fdb45fd0c10e37172e2ae30a03b631961ae6
giteup <dougharman@Douglass-MacBook-Pro.local>
2013-11-30 11:11:14 -0600

    2013_11_30 - Saturday 11:08am update
    I can add forms sequentially; howver, I can't get the input text to
    only appear in one textarea.  So, I decided to look at the "official"
    answer for help and learned that  / realized I should use "li" and that
    I can target the last one.

--- ShoppingList_2.js
+++ ShoppingList_2.js
@@ -1,4 +1,4 @@
-/* var validformat=/^\d{2}\/\d{2}\/\d{4}$/
+/*var validformat=/^\d{2}\/\d{2}\/\d{4}$/
 
 function validDate(toAdd_Date) {
     if (validDate == true) {
@@ -7,8 +7,25 @@ function validDate(toAdd_Date) {
     else {
         alert("Invalid Date Format.  Please correct and resubmit.");
     } 
-}  */
+}   */
 
+function newForm() {
+
+        $(".out_Form1").append('<fieldset class="fieldset1"><legend class="legend1"><input class="check" type="checkbox">Student Question or Note:</legend><br><textarea class="student"></textarea></fieldset>');
+
+        var toAdd_Text = $(".inputBox1").val();
+        $(".student").val(toAdd_Text);
+}
+
+
+/*function maxQuestion() {
+    if (question < 10) {
+
+    } 
+    else {
+        "Please contact your Mentor"
+    }
+}   */
 
 
 
@@ -24,47 +41,31 @@ $(document).ready(function () {
     $("#inputBox").keydown(function (event) {
         if (event.which == 13) {
             event.preventDefault();
+
+        
+
 /*      
-            
-            checkAnswer($("#inputBox").val());
+        validDate($(".in_dateBox1").val());
+            validDate($("#inputBox").val());
             showBanner($("#inputBox").val());
             showGuesses($("#inputBox").val());      */
-            var toAdd_Lesson = $(".in_lessonBox2").val();
-            $(".out_lessonBox2").append("<span>" + toAdd_Lesson + "</span>");
 
-            var toAdd_Date = $(".in_dateBox2").val();
-            $(".out_dateBox2").append("<span>" + toAdd_Date + "</span>");
-            
-            var toAdd_Text = $(".inputBox2").val();
-            $(".outputBox2").val(toAdd_Text);
+        var toAdd_Text = $(".inputBox2").val();
+        $(".outputBox1").val(toAdd_Text);
         }
     });
 
     // Save the student's input when they click submit
     $(".submitButton").click(function () {
         event.preventDefault();
-/*        checkAnswer($("#inputBox").val());
-        showBanner($("#inputBox").val());
-        showGuesses($("#inputBox").val());  
+
+        newForm();
+
+/*      validDate($(".in_dateBox1").val());
+            checkAnswer($("#inputBox").val());
+            showBanner($("#inputBox").val());
+            showGuesses($("#inputBox").val());  */
        
-        var toAdd_Lesson = $(".in_lessonBox2").val();
-        $(".lesson2").append("<span>" + toAdd_Lesson + "</span>");
-
-        var toAdd_Date = $(".in_dateBox2").val();
-        $(".date2").append("<span>" + toAdd_Date + "</span>");  */
-
-
-        $("form").addClass("out_Form1"); 
-        $("div1").addClass("fieldset1");
-        $("div1.fieldset1").replaceWith('<fieldset class="fieldset1"><legend class="legend1"><input class="check" type="checkbox">Student Question or Note:</legend><br><textarea class="outputBox1"></textarea></fieldset>');
-  
-      
-
-        var toAdd_Text = $(".inputBox1").val();
-        $(".outputBox1").val(toAdd_Text);     
-
-
-        
+/*           */
     });
-
 });
