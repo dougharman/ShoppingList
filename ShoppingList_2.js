8b5f9c3ab91c17525beaef84db97deb9d74af15d
giteup <dougharman@Douglass-MacBook-Pro.local>
2013-11-30 16:33:18 -0600

    2013_11_30 - Saturday 4:29pm update
    At wit's end once again.  This code targets all forms rather than just
    the one checked.  I could set up "hidden" forms and toggle; however,
    I'd like to do it on the run.  Here's the code that doesn't work:
    // To delete checked questions
        $("input").click(function() {
            $(this).closest("form").addClass("checked");
        });
    I am going to try a "for" function to number the question forms.  I
    suspect that means that I'll have to re-do my "nifty" newForm .append
    statement.  Somehow, I think I'm headed towards using an array.

--- ShoppingList_2.js
+++ ShoppingList_2.js
@@ -1,20 +1,19 @@
-/*var validformat=/^\d{2}\/\d{2}\/\d{4}$/
 
-function validDate(toAdd_Date) {
-    if (validDate == true) {
-        return toAdd_Date;
-    }
-    else {
-        alert("Invalid Date Format.  Please correct and resubmit.");
-    } 
-}   */
+
+
+
+
+
 
 function newForm() {
 
-        $(".out_Form1").append('<fieldset class="fieldset1"><legend class="legend1"><input class="check" type="checkbox">Student Question or Note:</legend><br><textarea class="student"></textarea></fieldset>');
-
-        var toAdd_Text = $(".inputBox1").val();
-        $(".student").val(toAdd_Text);
+        
+    
+    $(".out_Form1 li:last").append('<form><fieldset class="fieldset1"><legend class="legend1"><input class="studentQuestion" type="checkbox">Student Question or Note:</legend><textarea></textarea></fieldset></form>');
+    $("ul.out_Form1>li>form:last>fieldset>textarea").addClass("outputBox1");
+    var toAdd_Text = $(".inputBox1").val();
+    $("ul.out_Form1>li>form:last>fieldset>textarea").val(toAdd_Text);
+    $(".inputBox1").val("");  
 }
 
 
@@ -31,41 +30,46 @@ function newForm() {
 
 $(document).ready(function () {
 
+//  When a student checks the box "Mark Complete", the Working lesson closes and Review of Concepts opens in the first column
     $( ".review" ).accordion({collapsible: true});
 
     $(".finish").click(function() {
         $(".finish, .review").toggleClass("hide");
     });
 
-    // Save the student's input if they press enter instead of clicking submit 
-    $("#inputBox").keydown(function (event) {
-        if (event.which == 13) {
-            event.preventDefault();
 
-        
+// Validate the "To Be Completed by Date"                       Not sure how to validate the expression validDateFormat
+/*    $(".in_dateBox1").mouseleave(function() {
+        var toAdd_Date = $(".in_dateBox1").val();
+        var validDateFormat = /^\d{2}\d{2}\d{4}$/
 
-/*      
-        validDate($(".in_dateBox1").val());
-            validDate($("#inputBox").val());
-            showBanner($("#inputBox").val());
-            showGuesses($("#inputBox").val());      */
-
-        var toAdd_Text = $(".inputBox2").val();
-        $(".outputBox1").val(toAdd_Text);
+        if (validDateFormat == false) {
+            alert("Invalid Date Format.  Please correct.");
         }
-    });
+        else {
+            return toAdd_Date;
+        } 
+    });  */
 
-    // Save the student's input when they click submit
+// Save the student's Question or Note when they click submit
     $(".submitButton").click(function () {
         event.preventDefault();
 
-        newForm();
+// I'm not real sure I understand "!$.trim".  After looking at the "offical" answer, I Googled for examples of validating text and read the jQuery API
+        if (!$.trim($(".inputBox1").val())) {
+            alert("Please enter a Question or Note.");
+        } else {
+            newForm();
+        }
 
-/*      validDate($(".in_dateBox1").val());
-            checkAnswer($("#inputBox").val());
-            showBanner($("#inputBox").val());
-            showGuesses($("#inputBox").val());  */
-       
-/*           */
     });
+
+
+// To delete checked questions
+    $("input").click(function() {
+        $(this).closest("form").addClass("checked");
+    });
+
+
+
 });
