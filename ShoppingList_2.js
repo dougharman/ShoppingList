202f28b66c591cfee6711428f8aa21255902d483
giteup <dougharman@Douglass-MacBook-Pro.local>
2013-11-30 20:27:34 -0600

    2013_11_30 - Saturday 8:20pm update
    For the time being, I'm done with this.  I think I need to (a) I move
    on and (b) stick to the lesson plan next time.  I'm stuck!  I can't
    figure out how to turn my Question forms - created on the run - into an
    array.  More importantly, a numberical array so that I can delete them
    (by number) when checked.  When I test q[]; (my array), I get
    undefined.  q.length; returns a number so it's working.

--- ShoppingList_2.js
+++ ShoppingList_2.js
@@ -1,31 +1,23 @@
 
 
-
-
-
-
-
-function newForm() {
-
-        
-    
-    $(".out_Form1 li:last").append('<form><fieldset class="fieldset1"><legend class="legend1"><input class="studentQuestion" type="checkbox">Student Question or Note:</legend><textarea></textarea></fieldset></form>');
+function newForm(question) {
+    $(".out_Form1 li:last").append('<form class="form1"><fieldset class="fieldset1"><legend class="legend1"><input class="studentQuestion" type="checkbox">Student Question or Note number: </legend><textarea></textarea></fieldset></form>');
     $("ul.out_Form1>li>form:last>fieldset>textarea").addClass("outputBox1");
+
+    // Question number will equal index value in the array
+    var elements = $(document).get("form1");
+    var q = jQuery.makeArray(elements);
+
+    var numberOfQuestions = q.length;
+    var questionNumber = q.length[-1];
+
+    $("ul.out_Form1>li>form:last>fieldset>legend").append("<span>" + questionNumber + "</span>");
+
     var toAdd_Text = $(".inputBox1").val();
     $("ul.out_Form1>li>form:last>fieldset>textarea").val(toAdd_Text);
     $(".inputBox1").val("");  
+
 }
-
-
-/*function maxQuestion() {
-    if (question < 10) {
-
-    } 
-    else {
-        "Please contact your Mentor"
-    }
-}   */
-
 
 
 $(document).ready(function () {
@@ -38,7 +30,8 @@ $(document).ready(function () {
     });
 
 
-// Validate the "To Be Completed by Date"                       Not sure how to validate the expression validDateFormat
+// Validate the "To Be Completed by Date" 
+// Not sure how to validate the expression validDateFormat.  Code below doesn't work.                     
 /*    $(".in_dateBox1").mouseleave(function() {
         var toAdd_Date = $(".in_dateBox1").val();
         var validDateFormat = /^\d{2}\d{2}\d{4}$/
@@ -51,15 +44,26 @@ $(document).ready(function () {
         } 
     });  */
 
-// Save the student's Question or Note when they click submit
-    $(".submitButton").click(function () {
-        event.preventDefault();
-
+// Save the student's Question or Note if they press Enter rather than click Submit
+    $("#inputBox").keydown(function (event) {
+        if (event.which == 13) {
+            event.preventDefault();
 // I'm not real sure I understand "!$.trim".  After looking at the "offical" answer, I Googled for examples of validating text and read the jQuery API
         if (!$.trim($(".inputBox1").val())) {
             alert("Please enter a Question or Note.");
         } else {
-            newForm();
+            newForm($("#inputBox").val());
+        }
+
+    });
+
+// Save the student's Question or Note when they click submit
+    $(".submitButton").click(function () {
+        event.preventDefault();
+        if (!$.trim($(".inputBox1").val())) {
+            alert("Please enter a Question or Note.");
+        } else {
+            newForm($("#inputBox").val());
         }
 
     });
@@ -67,7 +71,9 @@ $(document).ready(function () {
 
 // To delete checked questions
     $("input").click(function() {
-        $(this).closest("form").addClass("checked");
+        event.preventDefault();
+//  This didn't work.  I need to access the form number assigned by the array.
+/*      $(this).closest("form").addClass("checked");  */
     });
 
 
