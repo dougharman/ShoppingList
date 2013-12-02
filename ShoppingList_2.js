1977d73031e42cd2142ffa1ec4d76fd7c71a419b
giteup <dougharman@Douglass-MacBook-Pro.local>
2013-12-01 20:04:37 -0600

    2013_12_1 Sunday 8:02pm
    Please see the *.js file.  I discovered the .toArray() method and was
    able to create an array of the forms.  However, I still can't assign a
    variable name to the array or, alternatively determine what index value
    the form is.

--- ShoppingList_2.js
+++ ShoppingList_2.js
@@ -4,14 +4,31 @@ function newForm(question) {
     $(".out_Form1 li:last").append('<form class="form1"><fieldset class="fieldset1"><legend class="legend1"><input class="studentQuestion" type="checkbox">Student Question or Note number: </legend><textarea></textarea></fieldset></form>');
     $("ul.out_Form1>li>form:last>fieldset>textarea").addClass("outputBox1");
 
-    // Question number will equal index value in the array
-    var elements = $(document).get("form1");
-    var q = jQuery.makeArray(elements);
+// Question number will equal index value in the array
+// Tied the following two lines with no success
+/*    var elements = $("ul.out_Form1").get("form1");
+    var q = jQuery.makeArray(elements); */
 
-    var numberOfQuestions = q.length;
-    var questionNumber = q.length[-1];
+// Next iteration - working from example for .toArray() found in jQuery api 
+// The statement $(".form1").toArray(); returns a horizontal array of three; placing it in brackets "[]" give me a vertical array.  I still
+// can't count it! var "q" comes back undefined. Also, q.length give "q" undefined
+/*      var q = [$(".form1").toArray()];
+        var numberOfQuestions = q.length;
+        var questionNumber = q.length[-1];
+*/
+// next try combine toArray and length
 
-    $("ul.out_Form1>li>form:last>fieldset>legend").append("<span>" + questionNumber + "</span>");
+    var numberOfQuestions = $(".form1").toArray().length;    // now, this works
+//  var questionNumber = $(".form1").toArray().length[-1];   // and this doesn't, so we'll replace questionNumber in the following with numberOfQuestion[-1]
+//  still not working
+
+//  work-around follows - it didn't work - responds NaN in legend line
+    $("ul.out_Form1>li>form:last>fieldset>legend").append("<span>" + ($(".form1").toArray().length)-1 + "</span>");
+
+//  While this hasn't worked - yet - I think I'm headed in the right direction.  My goal is to use the array index number as the question number.  Then,
+//  when a question is checked, I can delete it by array index number
+//  Questions:  How do I assign a variable name, e.g., "q", to the array created by .toArray(); alternatively does the method's output have a name
+//              so I can get the last item's index number 
 
     var toAdd_Text = $(".inputBox1").val();
     $("ul.out_Form1>li>form:last>fieldset>textarea").val(toAdd_Text);
@@ -54,7 +71,7 @@ $(document).ready(function () {
         } else {
             newForm($("#inputBox").val());
         }
-
+    }
     });
 
 // Save the student's Question or Note when they click submit
